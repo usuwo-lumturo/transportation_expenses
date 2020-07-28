cdfa84618c11a342a4cdfc6af6f6ebc39990b736
Natsumi Kadekari <kadekari.n@isoroot.jp>
2020-07-28 23:14:06 +0900

    ICカード読み取り画面Activity作成

---
+++ mobile_app/TransportationExpensesApp/app/src/main/java/com/example/transportationexpenses/IcReadActivity.java
@@ -0,0 +1,14 @@
+package com.example.transportationexpenses;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IcReadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ic_read);
+    }
+}
