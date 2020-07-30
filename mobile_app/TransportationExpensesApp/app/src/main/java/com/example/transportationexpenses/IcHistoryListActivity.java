300c4b8012ad8e035c3122d4f1b7cc23c36bba1f
Natsumi Kadekari <kadekari.n@isoroot.jp>
2020-07-30 19:33:07 +0900

    ICカード読み取り後のリスト画面Activity作成

---
+++ mobile_app/TransportationExpensesApp/app/src/main/java/com/example/transportationexpenses/IcHistoryListActivity.java
@@ -0,0 +1,29 @@
+package com.example.transportationexpenses;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.Objects;
+
+public class IcHistoryListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ic_history_list);
+
+        Toolbar toolbar = findViewById(R.id.ic_history_list_toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        RecyclerView recyclerView = findViewById(R.id.ic_history_list_recyclerview);
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(
+                this, DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(itemDecoration);
+    }
+}
