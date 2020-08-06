a34bb8b23861d50bb21a34bdb7efbfeaaddb1b1c
Natsumi Kadekari <kadekari.n@isoroot.jp>
2020-08-06 22:55:06 +0900

    保存確定時のトースト設定

--- mobile_app/TransportationExpensesApp/app/src/main/java/com/example/transportationexpenses/IcHistoryListCheckActivity.java
+++ mobile_app/TransportationExpensesApp/app/src/main/java/com/example/transportationexpenses/IcHistoryListCheckActivity.java
@@ -6,8 +6,12 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,6 +39,17 @@ public class IcHistoryListCheckActivity extends AppCompatActivity {
 
         IcHistoryListCheckAdapter icHistoryListCheckAdapter = new IcHistoryListCheckAdapter(createData());
         recyclerView.setAdapter(icHistoryListCheckAdapter);
+
+        Button saveButton = findViewById(R.id.ic_history_list_check_save_button);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IcHistoryListCheckActivity.this, MainActivity.class);
+                startActivity(intent);
+                Toast toast = Toast.makeText(getApplicationContext(), R.string.ic_history_list_check_save_toast, Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
     }
 
     @Override
