1a15b5c3d073305e29f98f9ee69a5ab41d0b94de
Natsumi Kadekari <kadekari.n@isoroot.jp>
2020-08-07 22:42:19 +0900

    勤務表作成画面Activity作成

---
+++ mobile_app/TransportationExpensesApp/app/src/main/java/com/example/transportationexpenses/IcHistoryTableMakeActivity.java
@@ -0,0 +1,14 @@
+package com.example.transportationexpenses;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IcHistoryTableMakeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ic_history_table_make);
+    }
+}
