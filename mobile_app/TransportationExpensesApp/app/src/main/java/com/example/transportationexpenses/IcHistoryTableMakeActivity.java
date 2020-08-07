d8427a786d8f24bcc13fe21943a64c197c98bdeb
Natsumi Kadekari <kadekari.n@isoroot.jp>
2020-08-07 22:45:09 +0900

    recyclerView追加とAdapter作成(Adapter未着手)

--- mobile_app/TransportationExpensesApp/app/src/main/java/com/example/transportationexpenses/IcHistoryTableMakeActivity.java
+++ mobile_app/TransportationExpensesApp/app/src/main/java/com/example/transportationexpenses/IcHistoryTableMakeActivity.java
@@ -1,8 +1,14 @@
 package com.example.transportationexpenses;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.Objects;
 
 public class IcHistoryTableMakeActivity extends AppCompatActivity {
 
@@ -10,5 +16,22 @@ public class IcHistoryTableMakeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ic_history_table_make);
+
+        Toolbar toolbar = findViewById(R.id.ic_history_table_make_toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        RecyclerView recyclerView = findViewById(R.id.ic_history_table_make_recyclerview);
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(
+                this, DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(itemDecoration);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        IcHistoryTableMakeAdapter icHistoryTableMakeAdapter = new IcHistoryTableMakeAdapter(createData());
+        recyclerView.setAdapter(icHistoryTableMakeAdapter);
+
     }
 }
