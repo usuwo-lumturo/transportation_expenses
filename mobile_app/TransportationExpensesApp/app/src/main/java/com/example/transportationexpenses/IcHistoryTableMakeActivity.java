412cdad9cc0a7e4d13146f6523fcbd828ea5ac99
Natsumi Kadekari <kadekari.n@isoroot.jp>
2020-08-10 22:57:37 +0900

    勤務表作成時のダイアログ仮設定

--- mobile_app/TransportationExpensesApp/app/src/main/java/com/example/transportationexpenses/IcHistoryTableMakeActivity.java
+++ mobile_app/TransportationExpensesApp/app/src/main/java/com/example/transportationexpenses/IcHistoryTableMakeActivity.java
@@ -1,13 +1,19 @@
 package com.example.transportationexpenses;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,6 +42,14 @@ public class IcHistoryTableMakeActivity extends AppCompatActivity {
         IcHistoryTableMakeAdapter icHistoryTableMakeAdapter = new IcHistoryTableMakeAdapter(createData());
         recyclerView.setAdapter(icHistoryTableMakeAdapter);
 
+        Button button = findViewById(R.id.ic_history_table_make_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buildAlertDialog();
+            }
+        });
+
     }
 
     @Override
@@ -44,6 +58,23 @@ public class IcHistoryTableMakeActivity extends AppCompatActivity {
             finish();
         }
         return true;
+    }
+
+    private void buildAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("勤務表作成確認")
+                .setMessage("の勤務表を作成します。よろしいですか？")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(IcHistoryTableMakeActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        Toast toast = Toast.makeText(getApplicationContext(), "勤務表の作成が完了しました。", Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                })
+                .setNegativeButton("キャンセル", null)
+                .show();
     }
 
     private List<IcHistory> createData() {
