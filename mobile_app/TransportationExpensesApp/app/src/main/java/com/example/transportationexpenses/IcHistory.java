38402f89e722bb9353e224129b14e37f2aee4edf
Natsumi Kadekari <kadekari.n@isoroot.jp>
2020-08-10 21:44:31 +0900

    変数名の追加

--- mobile_app/TransportationExpensesApp/app/src/main/java/com/example/transportationexpenses/IcHistory.java
+++ mobile_app/TransportationExpensesApp/app/src/main/java/com/example/transportationexpenses/IcHistory.java
@@ -6,16 +6,34 @@ public class IcHistory {
     private String date;
     /** 交通手段 */
     private String transportation;
-    /** 乗降車駅 */
-    private String boardingStation;
+    /** 乗車駅 */
+    private String gettingOnStation;
+    /** 降車駅 */
+    private String gettingOffStation;
+    /** 入線区 */
+    private String departureLine;
+    /** 出線区 */
+    private String arriveLine;
     /** 運賃 */
     private String fare;
+    /** 残高 */
+    private String balance;
+    /** 表示フラグ */
+    private boolean isHistoryVisible;
 
-    public IcHistory(String date, String transportation, String boardingStation, String fare) {
+    public IcHistory(String date, String transportation,
+                     String gettingOnStation, String gettingOffStation,
+                     String departureLine, String arriveLine, String fare,
+                     String balance, boolean isHistoryVisible) {
         this.date = date;
         this.transportation = transportation;
-        this.boardingStation = boardingStation;
+        this.gettingOnStation = gettingOnStation;
+        this.gettingOffStation = gettingOffStation;
+        this.departureLine = departureLine;
+        this.arriveLine = arriveLine;
         this.fare = fare;
+        this.balance = balance;
+        this.isHistoryVisible = isHistoryVisible;
     }
 
     public String getDate() {
@@ -26,11 +44,31 @@ public class IcHistory {
         return transportation;
     }
 
-    public String getBoardingStation() {
-        return boardingStation;
+    public String getGettingOnStation() {
+        return gettingOnStation;
+    }
+    
+    public String getGettingOffStation() {
+        return gettingOffStation;
+    }
+    
+    public String getDepartureLine() {
+        return departureLine;
+    }
+    
+    public String getArriveLine() {
+        return arriveLine;
     }
 
     public String getFare() {
         return fare;
+    }
+
+    public String getBalance() {
+        return balance;
+    }
+    
+    public boolean isHistoryVisible() {
+        return isHistoryVisible;
     }
 }
--- mobile_app/TransportationExpensesApp/app/src/main/java/com/example/transportationexpenses/IcReadHistoryListAdapter.java
+++ mobile_app/TransportationExpensesApp/app/src/main/java/com/example/transportationexpenses/IcReadHistoryListAdapter.java
@@ -3,6 +3,7 @@ package com.example.transportationexpenses;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -30,8 +31,12 @@ public class IcReadHistoryListAdapter extends RecyclerView.Adapter<IcReadHistory
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.dateText.setText(icHistoryList.get(position).getDate());
         holder.transportationText.setText(icHistoryList.get(position).getTransportation());
-        holder.stationsText.setText(icHistoryList.get(position).getBoardingStation());
+        holder.departureLineText.setText(icHistoryList.get(position).getDepartureLine());
+        holder.getOnStationText.setText(icHistoryList.get(position).getGettingOnStation());
+        holder.arriveLineText.setText(icHistoryList.get(position).getArriveLine());
+        holder.getOffStationText.setText(icHistoryList.get(position).getGettingOffStation());
         holder.fareText.setText(icHistoryList.get(position).getFare());
+        holder.historyVisibleCheckBox.setChecked(icHistoryList.get(position).isHistoryVisible());
     }
 
     @Override
@@ -43,15 +48,23 @@ public class IcReadHistoryListAdapter extends RecyclerView.Adapter<IcReadHistory
 
         private TextView dateText;
         private TextView transportationText;
-        private TextView stationsText;
+        private TextView departureLineText;
+        private TextView getOnStationText;
+        private TextView arriveLineText;
+        private TextView getOffStationText;
         private TextView fareText;
+        private CheckBox historyVisibleCheckBox;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             dateText = itemView.findViewById(R.id.list_ic_read_history_date);
             transportationText = itemView.findViewById(R.id.list_ic_read_history_transportation);
-            stationsText = itemView.findViewById(R.id.list_ic_read_history_stations);
+            departureLineText = itemView.findViewById(R.id.list_ic_read_history_departure_line);
+            getOnStationText = itemView.findViewById(R.id.list_ic_read_history_on_station);
+            arriveLineText = itemView.findViewById(R.id.list_ic_read_history_arrive_line);
+            getOffStationText = itemView.findViewById(R.id.list_ic_read_history_off_station);
             fareText = itemView.findViewById(R.id.list_ic_read_history_fare);
+            historyVisibleCheckBox = itemView.findViewById(R.id.list_ic_read_history_checkbox);
         }
     }
 }
