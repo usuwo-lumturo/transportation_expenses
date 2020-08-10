5ef57b588d4b5790b45247b97f65e6813abd7df7
Natsumi Kadekari <kadekari.n@isoroot.jp>
2020-08-10 22:59:49 +0900

    読み取り画面、読み取り後に遷移できるようにボタン配置(最後に消す)

--- mobile_app/TransportationExpensesApp/app/src/main/java/com/example/transportationexpenses/IcReadActivity.java
+++ mobile_app/TransportationExpensesApp/app/src/main/java/com/example/transportationexpenses/IcReadActivity.java
@@ -4,8 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.Objects;
 
@@ -20,6 +23,16 @@ public class IcReadActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        // TODO: 読み取りできるようになったら消す
+        Button button = findViewById(R.id.ic_read_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IcReadActivity.this, IcReadHistoryListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
